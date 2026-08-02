e3afd8004826498979ceeaba2deb803fd754c235
Kritsada Klaimak <ksdaklmk@gmail.com>
2026-08-02 10:52:56 +0700

    chore: remove duplicate projects import

--- feature/projects/src/main/kotlin/app/opentasks/feature/projects/ProjectsScreen.kt
+++ feature/projects/src/main/kotlin/app/opentasks/feature/projects/ProjectsScreen.kt
@@ -66,7 +66,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.key
