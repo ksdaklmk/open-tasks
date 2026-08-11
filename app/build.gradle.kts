8cecbcccb0a525f23ab01cd1b267b90f66347d8a
Kritsada Klaimak <ksdaklmk@gmail.com>
2026-08-11 21:44:54 +0700

    build: set version 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "app.opentasks"
         minSdk = 36
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
